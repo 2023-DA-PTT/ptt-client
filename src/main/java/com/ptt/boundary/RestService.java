5d3beaa5b6b036a049677c3bdfe6ab17745bd751
naichinger <aichinger.niklas@gmail.com>
2022-08-03 20:29:11 +0000

    update rest client path

--- src/main/java/com/ptt/boundary/RestService.java
+++ src/main/java/com/ptt/boundary/RestService.java
@@ -8,30 +8,29 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import java.util.List;
 
-@Path("plan/{planId}")
 @RegisterRestClient
 public interface RestService {
     @GET
+    @Path("plan/{planId}")
     PlanDto getPlanById(@PathParam("planId") long planId);
 
     @GET
-    @Path("step")
+    @Path("plan/{planId}/step")
     List<StepDto> getStepsByPlanId(@PathParam("planId") long planId);
 
     @GET
-    @Path("step/{stepId}/outputArgument")
+    @Path("plan/{planId}/step/{stepId}/outputArgument")
     List<OutputArgumentDto> getOutputArgumentsByStepId(@PathParam("planId") long planId, @PathParam("stepId") long id);
 
     @GET
-    @Path("step/{stepId}/inputArgument")
+    @Path("plan/{planId}/step/{stepId}/inputArgument")
     List<InputArgumentDto> getInputArgumentsByStepId(@PathParam("planId") long planId, @PathParam("stepId") long id);
 
     @GET
-    @Path("step/{stepId}/parameterRelation/from")
+    @Path("plan/{planId}/step/{stepId}/parameterRelation/from")
     List<StepParameterRelationDto> getStepParameterRelationByStepIdFrom(@PathParam("planId") long planId, @PathParam("stepId") long id);
 
     @GET
     @Path("planrun/{planrunid}")
-    PlanRunDto getPlanRunById(@PathParam("planrunid") long planId);
-
+    PlanRunDto getPlanRunById(@PathParam("planrunid") long planRunId);
 }
