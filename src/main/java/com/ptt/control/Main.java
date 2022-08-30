3c6cbd5075d2dcbcd3b19a3f51d5b1bee41df428
naichinger <aichinger.niklas@gmail.com>
2022-08-30 15:23:17 +0000

    change planrunid variable name

--- src/main/java/com/ptt/control/Main.java
+++ src/main/java/com/ptt/control/Main.java
@@ -9,7 +9,7 @@ public class Main {
   public static void main(String[] args) {
     SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
 
-    long planRunId = config.getValue("ptt-client.plan-run.id", Long.class);
+    long planRunId = config.getValue("test.plan-run.id", Long.class);
     int mqttPort = config.getValue("ptt-client.messaging.outgoing.measurements.port", Integer.class);
     String mqttAddress = config.getValue("ptt-client.messaging.outgoing.measurements.host", String.class);
 
